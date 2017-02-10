04ecb816d5f23216411dd1fd49e5e589db7e90c6
Raphael Hespanhol <Raphael Hespanhol@IBM377-PE013LA5.br.ibm.com>
2017-02-10 16:18:36 -0200

    Test Message

--- index.php
+++ index.php
@@ -12,21 +12,8 @@
 				<img class = 'newappIcon' src='images/newapp-icon.png'>
 			</td>
 			<td>
-				<h3 id = "message"> 
-<?php
-$servername = "us-cdbr-iron-east-04.cleardb.net";
-$username = "be8cdc50bb3240";
-$password = "8641e7fb";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-?> 				
+				<h3 id = "message"> <?php echo "Hello World!"; ?>
+ 				
 				</h3>
 				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
 			</td>
