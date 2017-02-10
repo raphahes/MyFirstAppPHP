250d6108cf93d3ff7389732c3b4e83e719454ad6
Raphael Hespanhol <Raphael Hespanhol@IBM377-PE013LA5.br.ibm.com>
2017-02-10 17:25:52 -0200

    update database

--- index.php
+++ index.php
@@ -13,23 +13,46 @@
 			</td>
 			<td>
 				<h3 id = "message">
-<?php
-echo "STEP 1: Set local variables";
-$servername = "us-cdbr-iron-east-04.cleardb.net:3306";
-$username = "be8cdc50bb3240";
-$password = "8641e7fb";
+				
 
-echo "<br>STEP 2: Register JDBC driver";
-$link = new mysql_connect($servername, $username, $password);
+<?php 
+$services = getenv("VCAP_SERVICES"); 
+$services_json = json_decode($services,true); 
+$mysql_config = $services_json["mysql-5.5"][0]["credentials"]; 
+$db = $mysql_config["name"]; 
+$host = $mysql_config["host"]; 
+$port = $mysql_config["port"]; 
+$username = $mysql_config["user"]; 
+$password = $mysql_config["password"];
 
-// Check connection
-if (!$link) {
-    die("Connection failed: " . mysql_error());
+$conn = mysql_connect($host . ':' . $port, $username, $password); 
+if(! $conn ) { die('Could not connect: ' . mysql_error()); } 
+mysql_select_db($db);
+
+$sql_create = 'CREATE TABLE FACTS(emp_name CHAR(30), emp_address CHAR(30), emp_salary INT, join_date DATETIME)'; 
+$retval = mysql_query($sql_create, $conn ); 
+if(! $retval ) { echo('Could not create database table: ' . mysql_error() . '<br/>'); 
+} else { echo "Created database table successfully<br/>"; }
+
+$sql_insert = 'INSERT INTO FACTS ' . '(emp_name,emp_address, emp_salary, join_date) '. 'VALUES ( "IBM BlueMix", "Rocks! X ", 1000, NOW() )';
+
+$retval = mysql_query($sql_insert, $conn ); 
+if(! $retval ) { die('Could not enter data: ' . mysql_error()); } 
+echo "Entered data successfully<br/>";
+
+$sql_read = 'SELECT * FROM FACTS ';
+
+$retval = mysql_query($sql_read, $conn );
+
+if(! $retval ) { die('Could not read FACTS table: ' . mysql_error()); } 
+while ($dbfield = mysql_fetch_assoc($retval)) { 
+	echo $dbfield['emp_name'] . '<br/>'; 
+	echo $dbfield['emp_address'] . $dbfield['emp_salary'] . '<br/>'; 
+	echo $dbfield['join_date'] . '<br/>'; 
 }
 
-mysql_close($link);
-echo "Connected successfully";
-?> 				
+mysql_close($conn); ?>
+				
 				</h3>
 				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
 			</td>
